df85cab9986b431d1e5ca8d66e666838d701c61a
Ashok <ashoknar@icloud.com>
2020-10-28 16:08:28 +0800

    Fixed pages not rendering, added missing .as_view() in urls

--- IS5009/urls.py
+++ IS5009/urls.py
@@ -19,11 +19,11 @@ from dashboard import views
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('dashboard/', views.DashboardView, name='dashboard'),
-    path('choose_fund/', views.ChooseFundView, name='choose_fund'),
-    path('general_fund/', views.GeneralFundView, name='general_fund'),
-    path('tech_fund/', views.TechFundView, name='tech_fund'),
-    path('login/', views.LoginView, name='login'),
-    path('fund-invest/', views.FundInvestView, name='fund-invest'),
+    path('dashboard/', views.DashboardView.as_view(), name='dashboard'),
+    path('choose_fund/', views.ChooseFundView.as_view(), name='choose_fund'),
+    path('general_fund/', views.GeneralFundView.as_view(), name='general_fund'),
+    path('tech_fund/', views.TechFundView.as_view(), name='tech_fund'),
+    path('login/', views.LoginView.as_view(), name='login'),
+    path('fund-invest/', views.FundInvestView.as_view(), name='fund-invest'),
     path('admin/', admin.site.urls),
 ]
